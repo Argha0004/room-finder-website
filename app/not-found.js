d9ae2a90220ab55bc4d573f9e5101198460aad53
Argha <arghadalal354@gmail.com>
2026-01-13 00:43:44 +0530

    Fix build issues and finalize UI & Supabase setup

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div style={{ color: "white", textAlign: "center", marginTop: "100px" }}>
+      <h1>404 – Page Not Found</h1>
+    </div>
+  );
+}
